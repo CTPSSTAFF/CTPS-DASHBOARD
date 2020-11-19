7c1e967c5acfe63dbdb590fccd3d3d2cf115f1f5
Ben Krepp <bkrepp@ctps.org>
2020-11-19 10:36:53 -0500

    Changes to app home page from Editorial group.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <head lang="en">
 <meta charset="utf-8">
 <link rel="icon" href="goo.gl/xQW9eP">
@@ -6,7 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" href="goo.gl/xQW9eP">
 
-<title>Performance Dashboard Transportation in the Boston Region</title>
+<title>Performance Dashboard Transportation in the Boston Region MPO</title>
 
 <!-- Font Awesome -->
 <script src="libs/font-awesome.js"></script>
@@ -107,7 +107,7 @@
 <!-- Desktop nav --> 
 <div class="col-md-6 desktop-nav">
 	<div class="col-md-11">
-		<div class="page_link"><a href="pages/crashes/index2.php" title="Go to crashes page" id="crashes">
+		<div class="page_link"><a href="pages/crashes/index2.php" title="Go to Crashes page" id="crashes">
 			<div class="col-md-12 col-sm-12 nav_metric">
 				<i class="fa fa-circle-o" aria-hidden="true"></i> Crashes </div>
 			<div class="col-md-6" data-toggle="tooltip" data-placement="left" title="Year of data: 2014">
@@ -117,7 +117,7 @@
 		</div>
 		<!-- End Crashes --> 
 
-		<div class="page_link"><a href="pages/congestion/index.php" title="Go to congestion page" id="congestion" >
+		<div class="page_link"><a href="pages/congestion/index.php" title="Go to Congestion page" id="congestion" >
 			<div class="col-md-12 col-sm-12 nav_metric">
 				<i class="fa fa-circle-o" aria-hidden="true"></i> Congestion </div>
 					<div class="col-md-6" data-toggle="tooltip" data-placement="left" title="Year of data: 2012; metric used - AM Speed Index">				
@@ -132,7 +132,7 @@
 		</div> 
 		<!-- End Congestion --> 
 
-		<div class="page_link"><a href="pages/bridges/index.php" title="Go to bridges page" id="bridges">
+		<div class="page_link"><a href="pages/bridges/index.php" title="Go to Bridges page" id="bridges">
 			<div class="col-md-12 col-sm-12 nav_metric">
 				<i class="fa fa-circle-o" aria-hidden="true"></i> Bridges</div>
 			<div class="col-md-6" data-toggle="tooltip" data-placement="left" title="Year of data: 2016">
@@ -145,7 +145,7 @@
 		</div>
 		<!-- End Bridges --> 
 
-		<div class="page_link"><a href="pages/pavement/index.php" title="Go to pavement condition page" id="pavement">
+		<div class="page_link"><a href="pages/pavement/index.php" title="Go to Pavement Condition page" id="pavement">
 			<div class="col-md-12 col-sm-12 nav_metric">
 				<i class="fa fa-circle-o" aria-hidden="true"></i> Pavement Condition</div>
 					<div class="col-md-6" data-toggle="tooltip" data-placement="left" title="Year of data: 2014">
@@ -159,7 +159,7 @@
 		</div>
 		<!-- End Pavement --> 
 
-		<div class="page_link"><a href="pages/sidewalks/index.php" title="Go to sidewalks page" id="sidewalks">
+		<div class="page_link"><a href="pages/sidewalks/index.php" title="Go to Sidewalks page" id="sidewalks">
 			<div class="col-md-12 col-sm-12 nav_metric">
 				<i class="fa fa-circle-o" aria-hidden="true"></i> Sidewalks</div>
 					<div class="col-md-6" data-toggle="tooltip" data-placement="left" title="Year of data: 2015">
@@ -172,7 +172,7 @@
 		</div>
 		<!-- End Sidewalk --> 
 
-		<div class="page_link"><a href="pages/bike_facilities/index.php" title="Go to bicycle facilities page" id="bikes">
+		<div class="page_link"><a href="pages/bike_facilities/index.php" title="Go to Bicycle Facilities page" id="bikes">
 			<div class="col-md-12 col-sm-12 nav_metric">
 				<i class="fa fa-circle-o" aria-hidden="true"></i> Bicycle Facilities</div>
 					<div class="col-md-6" data-toggle="tooltip" data-placement="left" title="Year of data: 2016">
@@ -184,7 +184,7 @@
 		</div>
 		<!-- End Bicycles--> 
 		
-		<div class="page_link"><a href="pages/demographics/index.php" title="Go to demographics page" id="demographics">
+		<div class="page_link"><a href="pages/demographics/index.php" title="Go to Demographics page" id="demographics">
 			<div class="col-md-12 col-sm-12 nav_metric">
 				<i class="fa fa-circle-o" aria-hidden="true"></i> Demographics </div>
 					<div class="col-md-6" data-toggle="tooltip" data-placement="left" title="Minority data from 2010; median household income data, vehicles per household data, unemployment data from 2010-2014 ACS">
@@ -196,7 +196,7 @@
 		</div>
 		<!-- End Demographics --> 
 
-		<div class="page_link"><a href="pages/about/index.php" title="Go to about page" id="about">
+		<div class="page_link"><a href="pages/about/index.php" title="Go to About page" id="about">
 			<div class="col-md-12 nav_metric">
 				<i class="fa fa-info-circle" aria-hidden="true"></i> About </div>
 				<div class="col-md-6">
@@ -208,7 +208,7 @@
 	</div>
 </div>
 <div class="footer">
-	<p> Background is a mapping of the Hispanic population in the Boston MPO region (data from 2010 Census). </p>
+	<p> Background is a map of the Hispanic population in the Boston Region MPO area based on data from the 2010 Census. </p>
 	<?php include 'components/footer.php';?>
 </div>
 </body>
