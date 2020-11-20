128618c802d298be4e0940a51d959430cb038900
Ben Krepp <bkrepp@ctps.org>
2020-11-20 09:07:43 -0500

    Incorporated changed text from Editorial group.

--- pages/pavement/index.php
+++ pages/pavement/index.php
@@ -38,19 +38,40 @@
 	</div> 
 <div id="header" class="col-md-10 col-md-offset-1">
 
-	<h1>Pavement Conditions</h1>
+	<h1>Pavement Condition</h1>
+	
+	<p>Freshly paved roads feel great under any set of wheels: automobile, motorcycle, bus, bicycle, scooter, or skateboard. 
+	  Unfortunately, roads in the Boston region steadily deteriorate with time, use, and New England weather. 
+	  Investments are made in roadway maintenance to extend pavement life and keep rides smooth. 
+	  Pavement condition is measured using the present serviceability index (PSI), which accounts for cracking, rutting,
+	  asphalt disintegration, and ride quality. 
+	  PSI scores range from zero (impassable) to five (perfectly smooth). 
+	</p>
 
-	<p>Freshly paved roads feel great under any set of wheels: automobile, motorcycle, bus, bicycle, scooter, or skateboard. Unfortunately, our roads steadily deteriorate with time, use, and New England weather. Investments are made in roadway maintenance to extend pavement life and keep rides smooth. Pavement condition is measured using the present serviceability index (PSI), a measure of a road’s roughness that accounts for patch work, rutting, and cracking. PSI scores range from zero (impassable) to five (perfectly smooth). Engineers consider a score of two or three as the minimum acceptable PSI.</p>
+	<p>The visualizations on this page show data from the Massachusetts Roadway Inventory for the years 2007 to 2014. 
+	   For more information about this performance measure and related data, visit the Pavement section on the About page.
+	</p>
 
 	<h2>Interstate Pavement</h2>
+	
 	<h3>Pavement Conditions in 2014</h3>
-	<p>Hover over the bars below to see the pavement condition of the five interstate highways in the Boston region, as of 2014. Conditions are displayed going northbound and southbound, or eastbound and westbound, as appropriate for each highway.  </p>
-
+	
+	<p>Hover over the bars below to see the pavement condition of the five interstate highways in the Boston region, as of 2014. 
+	   Conditions are displayed going northbound and southbound or eastbound and westbound, as appropriate for each highway. 
+	   The PSI thresholds for “excellent,” “good,” “fair,” and “poor” are more stringent for interstate highways compared
+	   to non-Interstate National Highway System roadways, which are shown on the Non-Interstate Pavement tab.
+	</p>
+	
 	<div id="chart" class="col-md-12"></div>
 
 	<h3>Trends</h3>
-	<p> Select an interstate highway below to learn how its pavement condition has changed over time. Each vertical line charts the PSI value of a segment of that interstate highway over the past decade.</p>
+	
+	<p>Click on an interstate highway below to learn how its pavement condition has changed over time. 
+	   Each vertical line charts the PSI value of a segment of that interstate highway over the past decade. 
+	</p>
+	
 	<p> Click to see only one interstate at a time. </p>
+	
 	<div class="col-md-12">
 		<button class = "all bigbutton timeline col-md-2" autofocus>All</button>
 		<button class = "I-90 bigbutton timeline col-md-2">I-90</button>
@@ -59,14 +80,7 @@
 		<button class = "I290 bigbutton timeline col-md-2">I-290</button>
 		<button class = "I495 bigbutton timeline col-md-2">I-495</button>
 	</div>
-	<div id="timeline" class="col-md-12"></div>
-	<!--
-	<div class = "col-md-12">
-		<h3>Does average daily traffic affect pavement condition?</h3>
-		<p> Average Daily Traffic, or ADT, is the average number of vehicles two-way passing a specific point in a 24-hour period. </p>
-	</div>
-	
-	<div id="adtgraph" class="col-md-12"></div>-->
+	<div id="timeline" class="col-md-12"></div>	
 	
 	<div class="footer col-md-12">
 		<?php include '../../components/footer.php';?>
